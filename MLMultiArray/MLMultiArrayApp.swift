3e8a26dcfa82e4244d22f45fe3394630a7d00f99
Kenny <kenny.langston@gmail.com>
2021-02-03 21:23:33 -0700

    Initial Commit

---
+++ MLMultiArray/MLMultiArrayApp.swift
@@ -0,0 +1,17 @@
+//
+//  MLMultiArrayApp.swift
+//  MLMultiArray
+//
+//  Created by Kenny Langston on 2/3/21.
+//
+
+import SwiftUI
+
+@main
+struct MLMultiArrayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
